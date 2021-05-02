34bbbd93c1630abc14f15bb5f73bf288f085f689
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2021-05-02 01:09:57 -0300

    add: service complaint

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -2,6 +2,7 @@ class ComplaintsController < ApplicationController
   def create
     company = Operations::Create::Company.new.run(complaint_params)
     locale = Operations::Create::Locale.new.run(complaint_params[:location], company[:id])
+    Operations::Create::Complaint.new.run(complaint_params, company[:id], locale[:id])
   end
 
   private
---
+++ app/services/operations/create/complaint.rb
@@ -0,0 +1,24 @@
+module Operations
+  module Create
+    class Complaint
+      def run(params, company_id, locale_id)
+        create(params, company_id, locale_id)
+      end
+
+      private
+
+      def create(params, company_id, locale_id)
+        ::Complaint.create(complait_params(params, company_id, locale_id))
+      end
+
+      def complait_params(params, company_id, locale_id)
+        {
+          title: params[:title],
+          description: params[:description],
+          company_id: company_id,
+          locale_id: locale_id
+        }
+      end
+    end
+  end
+end
