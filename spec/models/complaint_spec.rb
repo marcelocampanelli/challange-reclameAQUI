90a483b0a0831be05954bfcb4415471ef22b8cbe
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2021-05-02 21:17:24 -0300

    test: add test locale / complaints

--- spec/models/complaint_spec.rb
+++ spec/models/complaint_spec.rb
@@ -1,5 +1,32 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Complaint, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Complaint, type: :model do
+  context 'create a complaint' do
+    it 'is invalid' do
+      complaint = Complaint.new(
+        title: 'Rakuna Matata',
+        description: 'Lorem impsum Lorem impsum Lorem impsum  ',
+        company_id: 1,
+        locale_id: 3
+      )
+      expect(complaint).to_not be_valid
+    end
+
+    it 'is valid' do
+      company = Company.new(name: 'challange')
+      company.save
+
+      locale = Locale.new(id: 1, coordinates: '[-47.8536552, -24.4989961]', address: 'Rua B', state: 'São Paulo',
+                          city: 'Registro', country: 'Brazil', postcode: '11900-000', company_id: company.id)
+      locale.save
+      complaint = Complaint.new(
+        id: 1,
+        title: 'Rakuna Matata',
+        description: 'Lorem impsum Lorem impsum Lorem impsum  ',
+        company_id: company.id,
+        locale_id: locale.id
+      )
+      expect(complaint).to be_valid
+    end
+  end
+end
--- spec/models/locale_spec.rb
+++ spec/models/locale_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Locale, type: :model do
   context 'Create a locale' do
+    it 'is valid' do
+      company = Company.new(name: 'challange')
+      company.save
+
+      locale = Locale.new(id: 1, coordinates: '[-47.8536552, -24.4989961]', address: 'Rua B', state: 'São Paulo',
+                          city: 'Registro', country: 'Brazil', postcode: '11900-000', company_id: company.id)
+      expect(locale).to be_valid
+    end
+
     it 'is invalid' do
-      locale = Locale.new(coordinates: '[-47.8536552, -24.4989961]', address: 'Rua B', state: 'São Paulo',
-                          city: 'Registro', country: 'Brazil', postcode: '11900-000', company_id: 2)
+      company = Company.new(name: 'challange')
+
+      locale = Locale.new(id: 1, coordinates: '[-47.8536552, -24.4989961]', address: 'Rua B', state: 'São Paulo',
+                          city: 'Registro', country: 'Brazil', postcode: '11900-000', company_id: company.id)
       expect(locale).to_not be_valid
     end
   end
